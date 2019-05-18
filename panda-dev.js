facd72e9fe5efc96f3b59061dd98c5413a3d9b08
21126881 <lowb@mail.com>
2019-05-18 23:03:30 +0100

    Update panda-dev.js

--- panda-dev.js
+++ panda-dev.js
@@ -183,6 +183,7 @@ sniff.send(null);
 xhr.send(null);
 
 };
+console.log(window.location.href.match(/^https?:\/\/exhentai\.org\/fullimg\.php/i));
 if(document.getElementById('panda_plus')){console.log('exist');}
 else if(document.domain!='exhentai.org'){if(confirm(panda_lang_a002)){window.location.href='https://exhentai.org/favicon.ico';}}
 else if(window.location.href.match(/^https?:\/\/exhentai\.org\/fullimg\.php/i) && document.documentElement.outerHTML=='err'){panda_hackfull();}

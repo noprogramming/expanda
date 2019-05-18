a58fbec6d6497fa2ae87229de65b8ea2ccbe1a36
21126881 <lowb@mail.com>
2019-05-18 23:41:27 +0100

    Update panda-dev.js

--- panda-dev.js
+++ panda-dev.js
@@ -171,7 +171,9 @@ sniff.open('GET',window.location.href,true);
 sniff.onerror=function(){}; //恢复账号先！
 sniff.onreadystatechange=function(){
 if(this.readyState==this.HEADERS_RECEIVED){
+console.log(sniff.getAllResponseHeaders());
 console.log(this.getAllResponseHeaders());
+
 }};
 sniff.send(null);
 

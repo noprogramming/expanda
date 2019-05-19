641c0b6180f06262e9175f5c89fdd85ec1714b40
21126881 <lowb@mail.com>
2019-05-19 14:56:38 +0100

    Update panda-dev.js

--- panda-dev.js
+++ panda-dev.js
@@ -207,11 +207,5 @@ var panda_sniff={};
 var exkey_sniffing,exkey_user,exkey_pass,exkey_igneous,exkey_sk; ////最好不用公共变量（同上写入COOKIE）
 if(document.domain!='exhentai.org'){if(confirm(panda_lang_a002)){window.location.href='https://exhentai.org/favicon.ico';}}
 else if(document.getElementById('gdt') && !document.getElementById('panda_plus')){panda_plusfunc();}
-else if(window.location.pathname=='/fullimg.php' && document.documentElement.outerHTML.match(/err/)){panda_sniffimg(true,function(){
-var img=new Image();
-img.src=window.location.href;
-console.log('load');
-img.onerror=function(){console.log('not');panda_recookie();};
-img.onload=function(){console.log('right');document.body.innerHTML='<img src="'+img.src+'" alt="Retry" style="max-width:100%;" />';panda_recookie();};
-});}
+else if(window.location.pathname=='/fullimg.php' && document.documentElement.outerHTML.match(/err/)){panda_sniffimg(true,function(){var img=new Image();img.src=window.location.href;img.onerror=function(){panda_recookie();};img.onload=function(){document.body.innerHTML='<img src="'+img.src+'" alt="Retry" style="max-width:100%;" />';panda_recookie();};});}
 else if(window.location.pathname=='/favicon.ico' || document.contentType=='image/gif'){panda_leapover(panda.getAttribute('exkey'));}; ////防止重复刷新？

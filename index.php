46dd17dccb3efd85812b9fdaed3aca1a5e12af67
21126881 <lowb@mail.com>
2019-05-22 23:49:08 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+$z='panda.gxtel.com';
 preg_match('/^\/(\w+)?\.?panda.user.js$/i',$_SERVER['REQUEST_URI'],$q);
 $k=(empty($q['1'])||strlen($q['1'])<36||!is_numeric(substr(explode('x',$q['1'])['0'],32)))?null:$q['1'];
 header('Content-Type:application/javascript;charset=utf-8;');
 echo "
 // ==UserScript==
 // @name         熊猫书签
-// @namespace    Panda_Bookmark
+// @namespace    https://".$z."
 // @description  zh-cn/
 // @license      MIT
 // @version      11
@@ -15,7 +16,7 @@ echo "
 (function(){
 'use strict';
 if(window.location.host.match(/^\w+\.exhentai\.org$/i)){window.location.href='https://exhentai.org/favicon.ico';return;};
-var a=document.createElement('script');a.src='//panda.gxtel.com/panda.js?'+parseInt(Date.parse(new Date())/600000);".($k?'a.setAttribute(\'exkey\',\''.$k.'\');':'')."document.body.appendChild(a);
+var a=document.createElement('script');a.src='//".$z."/panda.js?'+parseInt(Date.parse(new Date())/600000);".($k?'a.setAttribute(\'exkey\',\''.$k.'\');':'')."document.body.appendChild(a);
 })();
 ";
 ?>

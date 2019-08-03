773cb7a5484f5e8e266353e96fe5c4c548b6a286
ehpanda <53454377+ehpanda@users.noreply.github.com>
2019-08-03 21:24:07 +0100

    Add files via upload

--- panda.php
+++ panda.php
@@ -7,13 +7,14 @@ echo "
 // @namespace    https://".$_SERVER['HTTP_HOST']."
 // @description  zh-cn/
 // @license      MIT
-// @version      12
-// @match        *.exhentai.org/*
+// @version      13
+// @match        ex.com
+// @match        exhentai.org/*
 // @grant        none
 // ==/UserScript==
 (function(){
 'use strict';
-if(window.location.host.match(/^\w+\.exhentai\.org$/i)){window.location.href='https://exhentai.org/favicon.ico';return;};
+if(window.location.host=='ex.com'){window.location.href='https://exhentai.org/favicon.ico';return;};
 var a=document.createElement('script');a.src='//".$_SERVER['HTTP_HOST']."/panda.js?'+parseInt(Date.parse(new Date())/600000);".($key?"a.setAttribute('exkey','".$key['1']."');":"")."document.body.appendChild(a);
 })();
 ";

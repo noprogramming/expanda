937fdf96d48205f8267ced058259449a877a6a22
noprogramming <53454377+noprogramming@users.noreply.github.com>
2020-01-07 19:53:05 +0800

    Update layer.js

--- api/layer.js
+++ api/layer.js
@@ -1,4 +1,4 @@
-let obj=require(require('path').resolve(__dirname,'..')+'/config','utf-8');
+let obj=JSON.stringify(require(require('path').resolve(__dirname,'..')+'/config','utf-8'));
 console.log(obj);
 module.exports=function(req,res){
 let key=req.query.key.replace(/[^\w]+/ig,'');

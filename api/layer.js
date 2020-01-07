06767484b1c5e6c075a8d47ac453ee95e15a83ea
noprogramming <53454377+noprogramming@users.noreply.github.com>
2020-01-08 02:38:12 +0800

    Add files via upload

--- api/layer.js
+++ api/layer.js
@@ -5,7 +5,7 @@
 */
 module.exports=function(req,res){
 let qry=Object.keys(req.query)[0].match(/^(\w+)\.panda\.user\.js$/i);
-let key=qry[1]?qry[1]:null;
+let key=qry?qry[1]:null;
 let obj=JSON.parse(require('fs').readFileSync(require('path').resolve(__dirname,'..')+'/config','utf-8'));
 let txt=obj.monkey.replace('`func`',obj.func).replace('`cdn`',obj.cdn).replace('`ver`',obj.ver).replace('`key`',(key?'s.setAttribute(\'exkey\',\''+key+'\');':'')).replace('`web`','https://'+window.location.hostname+window.location.pathname.replace(/[\/]+$/g,'/'));
 res.status(200).send(txt);

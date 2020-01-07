811d149c79ecc6f65361f073e89369be553a6a32
noprogramming <53454377+noprogramming@users.noreply.github.com>
2020-01-07 20:12:33 +0800

    Update layer.js

--- api/layer.js
+++ api/layer.js
@@ -1,5 +1,5 @@
 let obj=require('fs').readFileSync(require('path').resolve(__dirname,'..')+'/config','utf-8');
-console.log(JSON.parse(obj));
+console.log(obj.toString());
 module.exports=function(req,res){
 let key=req.query.key.replace(/[^\w]+/ig,'');
 res.status(200).send('Key:'+key+':'+obj.ver);

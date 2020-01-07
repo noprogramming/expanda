f41493f3fdb3938a6eff87feae21d0953abb0c9e
noprogramming <53454377+noprogramming@users.noreply.github.com>
2020-01-08 02:43:27 +0800

    Add files via upload

--- api/layer.js
+++ api/layer.js
@@ -4,9 +4,10 @@
 --Now.sh|now.json--
 */
 module.exports=function(req,res){
+let url=URL.parse(req.url);
 let qry=Object.keys(req.query)[0].match(/^(\w+)\.panda\.user\.js$/i);
 let key=qry?qry[1]:null;
 let obj=JSON.parse(require('fs').readFileSync(require('path').resolve(__dirname,'..')+'/config','utf-8'));
-let txt=obj.monkey.replace('`func`',obj.func).replace('`cdn`',obj.cdn).replace('`ver`',obj.ver).replace('`key`',(key?'s.setAttribute(\'exkey\',\''+key+'\');':'')).replace('`web`','https://'+window.location.hostname+window.location.pathname.replace(/[\/]+$/g,'/'));
+let txt=obj.monkey.replace('`func`',obj.func).replace('`cdn`',obj.cdn).replace('`ver`',obj.ver).replace('`key`',(key?'s.setAttribute(\'exkey\',\''+key+'\');':'')).replace('`web`','https://'+url.hostname+url.pathname.replace(/[\/]+$/g,'/'));
 res.status(200).send(txt);
 }
